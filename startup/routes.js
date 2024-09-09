081bd1c5ba058e9c8a100e85007f0719e996c464
Elksass315 <qsasomar@gmail.com>
2024-09-09 23:21:22 +0300

    Weather routes

--- startup/routes.js
+++ startup/routes.js
@@ -1,4 +1,9 @@
 const express = require('express');
+
+const weatherRouter = require('../routes/weather');
+const userRouter = require('../routes/user');
+const authRouter = require('../routes/auth');
+
 const errorMiddleware = require("../middleware/error");
 
 module.exports = function (app) {
@@ -10,6 +15,9 @@ module.exports = function (app) {
     app.get('/', (req, res) => {
         res.send('welcome to the NODE_WEATHER_API!');
     });
+    app.use('/api/weather', weatherRouter);
+    app.use('/api/user', userRouter);
+    app.use('/api/auth', authRouter);
 
     app.use(errorMiddleware)
     };
