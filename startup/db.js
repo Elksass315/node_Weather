097d7693ff41a0c9254d738adbe0f262ecc82eeb
Elksass315 <qsasomar@gmail.com>
2024-09-12 00:45:23 +0300

    Fix db connection error

--- startup/db.js
+++ startup/db.js
@@ -3,6 +3,10 @@ const config = require("config");
 const winston = require("winston");
 
 module.exports = function () {
-    mongoose.connect(config.get("db"))
-        .then(() => winston.info(`DB Connected to ${config.get("db")}`));
+    mongoose.connect(config.get("db"),
+    ).then(() => {
+        console.log('Connected to MongoDB');
+    }).catch((err) => {
+        console.error('Error connecting to MongoDB:', err.message);
+    });
 }
