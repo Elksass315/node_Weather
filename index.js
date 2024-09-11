cd683b591735f1e7e9d164651f9e67d817bfef6d
Elksass315 <qsasomar@gmail.com>
2024-09-12 00:46:19 +0300

    New: project unit test

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const config = require('config');
 const app = express();
 
 require('./startup/routes')(app);
+require('./startup/db')();
+// require('./startup/logging')();
 
 const port = process.env.PORT || config.get('port') || 3000;
 
