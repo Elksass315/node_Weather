663153ce71c900f4351e4540a040d026e6c360a9
Elksass <qsasomar@gmail.com>
2024-10-13 02:16:06 +0300

    New: swagger Api docs

--- startup/routes.js
+++ startup/routes.js
@@ -4,10 +4,20 @@ import userRouter from '../routes/user.js';
 import authRouter from '../routes/auth.js';
 import favoriteRouter from '../routes/favorite.js';
 import weatherRouter from '../routes/weather.js';
+import swaggerUi from 'swagger-ui-express';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const swaggerDocument = JSON.parse(fs.readFileSync(path.join(__dirname, '../swagger.json'), 'utf8'));
 
 export default function (app) {
     app.use(express.json());
-    
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
     app.get('/', (req, res) => {
         res.send('welcome to the NODE_WEATHER_API!');
     });
